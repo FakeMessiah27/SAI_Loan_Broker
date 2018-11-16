abdf66a98df187e53941a336023f97a817b64ba9
Thomas <thomas.vaniersel@hotmail.com>
2018-11-16 09:58:40 +0100

    Initial commit

---
+++ loan-broker-application/src/main/java/loanBrokerApplication/gui/LoanBrokerController.java
@@ -0,0 +1,5 @@
+package loanBrokerApplication.gui;
+
+public class LoanBrokerController {
+
+}
---
+++ loan-broker-application/src/main/java/loanBrokerApplication/gui/LoanBrokerMain.java
@@ -0,0 +1,33 @@
+package loanBrokerApplication.gui;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class LoanBrokerMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("loanBrokerApplication.fxml"));
+        primaryStage.setTitle("Loan Broker");
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
